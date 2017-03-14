73356853b8bf77b0f53c437eacbeec35db6fe2cf
Ximin Luo <infinity0@debian.org>
2017-03-14 22:54:40 +0100

    Work around older versions of nodejs returning different codes

--- consume/pecsplit.js
+++ consume/pecsplit.js
@@ -4,7 +4,7 @@
 
 var unquote = function(x) {
   if (x.search(/%[^#+.]|%$/) >= 0)
-    throw "invalid value: bad escape: " + x;
+    throw new Error("invalid value: bad escape: " + x);
   return x.replace(/%\./g, ':').replace(/%\+/g, '=').replace(/%#/g, '%');
 };
 
@@ -12,7 +12,7 @@ var parse_prefix_map = function(x) {
   return (x || "").split(/:/g).filter(Boolean).map(function(part) {
     var tuples = part.split(/=/g).map(unquote);
     if (tuples.length !== 2)
-      throw "invalid value: not a pair: " + tuples;
+      throw new Error("invalid value: not a pair: " + tuples);
     return tuples;
   });
 };
@@ -32,8 +32,13 @@ var map_prefix = function(string, pm) {
 
 // Main program
 
-var pm = parse_prefix_map(process.env["BUILD_PATH_PREFIX_MAP"]);
-// var i = 2 is just how nodejs yolos its way through common conventions
-for (var i = 2, l = process.argv.length; i < l; ++i) {
-  console.log(map_prefix(process.argv[i], pm));
+try {
+  var pm = parse_prefix_map(process.env["BUILD_PATH_PREFIX_MAP"]);
+  // var i = 2 is just how nodejs yolos its way through common conventions
+  for (var i = 2, l = process.argv.length; i < l; ++i) {
+    console.log(map_prefix(process.argv[i], pm));
+  }
+} catch (e) {
+  console.error(e.stack);
+  process.exit(1); // older versions returned 8, messing up our CI testing
 }
