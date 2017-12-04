8d4ceed752d4f3a4ef6548bf1b7045c1c4fc464f
Ximin Luo <infinity0@debian.org>
2017-12-04 15:51:29 +0100

    Fix printing of multiline byte output, spotted by Gabriel Scherer

--- consume/make_printable.py
+++ consume/make_printable.py
@@ -6,12 +6,14 @@ import textwrap
 
 sanitized = [x for x in string.printable if x not in string.whitespace or x in " \n"]
 
-contents = open(sys.argv[1], 'rb').read().rstrip(b'\n')
-try:
-    contents = contents.decode("utf-8")
-    if all(x in sanitized for x in contents):
-        print(contents)
-    else:
-        print("\u200b".join(repr(contents)))
-except UnicodeDecodeError:
-    print("".join(repr(contents)))
+contents = open(sys.argv[1], 'rb').readlines()
+for line in contents:
+    line = line.rstrip(b'\n')
+    try:
+        line = line.decode("utf-8")
+        if all(x in sanitized for x in line):
+            print(line)
+        else:
+            print("\u200b".join(repr(line)))
+    except UnicodeDecodeError:
+        print("".join(repr(line)))
